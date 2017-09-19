fe65e4cd09d300253f003603764aea20d758a784
Jason L <jasonliu1999@gmail.com>
2017-09-18 20:57:06 -0400

    Moved all api based functions to separate class

--- src/main/java/UploadClass.java
+++ src/main/java/UploadClass.java
@@ -1,27 +1,7 @@
 //package coreapp;
 //
-//import com.google.api.client.auth.oauth2.Credential;
-//import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-//import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-//import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-//import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-//import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-//import com.google.api.client.http.HttpTransport;
-//import com.google.api.client.json.jackson2.JacksonFactory;
-//import com.google.api.client.json.JsonFactory;
-//import com.google.api.client.util.store.FileDataStoreFactory;
 //
-//import com.google.api.services.drive.DriveScopes;
-//import com.google.api.services.drive.model.*;
-//import com.google.api.services.drive.Drive;
-//
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.io.InputStreamReader;
-//import java.util.Arrays;
-//import java.util.List;
-//
-//public class UploadClass {
+//public class UploadClass extends DriveSync {
 //
 //    public static void uploadFile(Drive driveService)
 //    {
